5b214025600af315a8fe6c9c0b16b2f28593e010
likhithar2507 <73028560+likhithar2507@users.noreply.github.com>
2022-03-02 20:26:05 +0530

    Create program4.py

---
+++ program4.py
@@ -0,0 +1,4 @@
+# WAP to display the smallest and largest word from the string.
+User_input = input("Enter  the phrase: ")
+print("Smallest= ", min(User_input).upper())
+print("Largest= ", max(User_input).upper())
