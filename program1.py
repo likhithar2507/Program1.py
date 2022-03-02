1f4f9dee5656c633ec71d2f65cdff5056aaf71b9
likhithar2507 <73028560+likhithar2507@users.noreply.github.com>
2022-03-02 20:16:31 +0530

    Create program1.py

---
+++ program1.py
@@ -0,0 +1,9 @@
+# WAP to test whether an input is integer or not.
+# user input
+user_input = input("Enter a value: ")
+# checking to see if the value is an integer
+
+if User_input.isdigit():
+    print("The value is an integer")
+else:
+    print("The value is NOT an integer")
